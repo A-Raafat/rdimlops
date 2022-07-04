e05af5a26957e3178ded3a37fb0d788f85dab4f2
Ahmed Raafat <36015119+A-Raafat@users.noreply.github.com>
2022-07-04 12:40:27 +0200

    Update test_clf.py

--- test_clf.py
+++ test_clf.py
@@ -1,12 +1,13 @@
 import joblib
 import lightgbm
+import cv2
 
 def preprocess(im):
 	im = cv2.resize(im,(150,150))/255.0
 	im = im.reshape(1,-1)
 	return im
 	
-im_path = 'test_images/norm.jpg'
+im_path = 'static/images/covid.jpg'
 
 im = cv2.imread(im_path,0)
 im = preprocess(im)
