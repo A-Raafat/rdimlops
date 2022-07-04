527f8064c417f3d030983a3359620f7f57078a02
Ahmed Raafat <36015119+A-Raafat@users.noreply.github.com>
2022-07-04 17:15:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,32 +17,16 @@ templates = Jinja2Templates(directory="templates")
 def load_clf():
     # Load classifier from pickle file
     global clf
-    clf = joblib.load("app/model.pkl")
+    try:
+    	clf = joblib.load("app/model.pkl")
+    except:
+        clf = joblib.load("model.pkl")
 
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return 'Welcome to Covid classifier by Ahmed Raafat, Please visit localhost/docs for the fastapi UI'
 
-
-@app.post("/")
-async def home_predict(request: Request, file: UploadFile = File(...) ):
-    
-    #img = cv2.imread(file.filename)
-    img_bytes=file.file.read()
-    nparr = np.fromstring(img_bytes, np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image,(150,150))/255.0
-    image = image.reshape(1,-1)
-    pred = clf.predict(image)[0]
-    
-    if pred < 0.5:
-    	out="Congratulations you are Normal !"
-    else:
-    	out="Unfortunately you are infected by Covid"
-    	
-    result={"Prediction": out}
-    return templates.TemplateResponse("index.html", {"request": request, 'result': result})
     
 @app.post("/predict")
 async def predict(file: UploadFile = File(...) ):
@@ -57,10 +41,10 @@ async def predict(file: UploadFile = File(...) ):
     
     if pred < 0.5:
     	out="Normal"
+    	ss= round(((1-pred)*100),2)
     else:
     	out="Covid"
+    	ss = round(pred*100,2)
+    result={"Prediction": out, "Score" : str(ss)+'%'}
     
-    return {"Prediction": out}
-    
-    
-    
+    return {"result": result}
