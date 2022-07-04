ff20676fd5fb349e3db455a9d63c225830e53a4c
Ahmed Raafat <36015119+A-Raafat@users.noreply.github.com>
2022-07-04 02:37:07 +0200

    Update test_clf.py

--- test_clf.py
+++ test_clf.py
@@ -1,5 +1,21 @@
 import joblib
+import lightgbm
+
+def preprocess(im):
+	im = cv2.resize(im,(150,150))/255.0
+	im = im.reshape(1,-1)
+	return im
+	
+im_path = 'test_images/norm.jpg'
+
+im = cv2.imread(im_path,0)
+im = preprocess(im)
 
 def test_mdl_sanity():
 
     assert joblib.load('model.pkl)
+                       
+                   
+def test_mdl_prediction():
+    clf = joblib.load('model.pkl)
+    assert clf.predict(im)
