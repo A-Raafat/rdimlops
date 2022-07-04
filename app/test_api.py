9ea2cc80c7d8a179d3eac927a1296cff5c4545e6
Ahmed Raafat <36015119+A-Raafat@users.noreply.github.com>
2022-07-04 13:11:02 +0200

    Add files via upload

---
+++ app/test_api.py
@@ -0,0 +1,22 @@
+import joblib
+import lightgbm
+import cv2
+
+def preprocess(im):
+	im = cv2.resize(im,(150,150))/255.0
+	im = im.reshape(1,-1)
+	return im
+	
+im_path = 'app/static/images/covid.jpg'
+
+im = cv2.imread(im_path,0)
+im = preprocess(im)
+
+def test_mdl_sanity():
+
+    assert joblib.load('model.pkl')
+                       
+                   
+def test_mdl_prediction():
+    clf = joblib.load('model.pkl')
+    assert clf.predict(im)
